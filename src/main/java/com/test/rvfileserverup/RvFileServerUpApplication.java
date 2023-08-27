23e62ab7d7a8b445983cbf42dde9a0f3ced45c89
basfar <mukhtaronif@gmail.com>
2023-08-27 21:04:36 +0100

    Updated

---
+++ src/main/java/com/test/rvfileserverup/RvFileServerUpApplication.java
@@ -0,0 +1,13 @@
+package com.test.rvfileserverup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvFileServerUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RvFileServerUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/rvfileserverup/RvFileServerUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rvfileserverup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvFileServerUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
