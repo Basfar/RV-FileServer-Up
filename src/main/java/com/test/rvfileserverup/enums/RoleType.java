d0fed1009b2f58b64407355affd6ac06c9065824
basfar <mukhtaronif@gmail.com>
2023-09-09 11:56:26 +0100

    Updated

---
+++ src/main/java/com/test/rvfileserverup/enums/RoleType.java
@@ -0,0 +1,7 @@
+package com.test.rvfileserverup.enums;
+
+public enum RoleType {
+    USER,
+
+    AMDIN
+}
--- src/main/java/com/test/rvfileserverup/model/User.java
+++ src/main/java/com/test/rvfileserverup/model/User.java
@@ -1,11 +1,14 @@
 package com.test.rvfileserverup.model;
 
+import com.test.rvfileserverup.enums.RoleType;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -18,39 +21,52 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String firstname;
+
     private String lastname;
+
     private String email;
+
     private String password;
+
     private boolean verified;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
